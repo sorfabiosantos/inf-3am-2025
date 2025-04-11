56419e87c39919c50cc3c5f307af6108273116c0
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-04-11 08:29:49 -0300

    Exercício API Loja de Departamentos

--- source/Models/Store/Product.php
+++ source/Models/Store/Product.php
@@ -2,7 +2,67 @@
 
 namespace Source\Models\Store;
 
-class Product
+use Source\Core\Model;
+
+class Product extends Model
 {
+    private $id;
+    private $idCategory;
+    private $name;
+    private $price;
+
+    public function __construct(
+        int $id = NULL,
+        int $idCategory = NULL,
+        string $name = NULL,
+        float $price = NULL
+    )
+    {
+        $this->table = "products";
+        $this->id = $id;
+        $this->idCategory = $idCategory;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getIdCategory(): ?int
+    {
+        return $this->idCategory;
+    }
+
+    public function setIdCategory(?int $idCategory): void
+    {
+        $this->idCategory = $idCategory;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): void
+    {
+        $this->price = $price;
+    }
 
 }
--- source/Models/Store/ProductCategory.php
+++ source/Models/Store/ProductCategory.php
@@ -2,7 +2,41 @@
 
 namespace Source\Models\Store;
 
-class ProductCategory
+use Source\Core\Model;
+
+class ProductCategory extends Model
 {
+    private $id;
+    private $name;
+
+    public function __construct(
+        int $id = NULL,
+        string $name = NULL
+    )
+    {
+        $this->table = "products_categories";
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
 
 }
